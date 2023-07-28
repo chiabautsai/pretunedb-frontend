05682054d733064f11cb8a7233f97e70feb58d94
Chiabau Tsai <chiabau@Chiabaus-Air.lan>
2023-07-28 14:48:11 +0800

    changed env variable name to start with REACT_APP_

--- src/contexts/ApiContext.js
+++ src/contexts/ApiContext.js
@@ -4,8 +4,7 @@ export const ApiContext = createContext();
 
 const ApiContextProvider = ({ children }) => {
   // Set a default value if the env variable is not defined
-  const [apiUrlBase, setApiUrlBase] = useState(process.env.API_URL_BASE || 'http://localhost:8787/api');
-
+  const [apiUrlBase, setApiUrlBase] = useState(process.env.REACT_APP_API_URL_BASE || 'http://localhost:3001/api');
   return (
     <ApiContext.Provider value={{ apiUrlBase, setApiUrlBase }}>
       {children}
